0cab7d7f97f3c609c76b3ed3249e828c7766cfdc
Mark J. Williamson <mw529@cam.ac.uk>
2021-08-05 13:53:00 +0100

    Add quad example

--- hellopythonworld/hellopythonworld.py
+++ hellopythonworld/hellopythonworld.py
@@ -1,6 +1,11 @@
+import math
+
 class HelloPythonWorld:
     def square(self, x):
         return x * x
 
     def cube(self, x):
         return x * x * x
+
+    def quad(self, x):
+        return math.pow(x, 4)
--- hellopythonworld/tests/unit/test_hellopythonworld.py
+++ hellopythonworld/tests/unit/test_hellopythonworld.py
@@ -8,3 +8,7 @@ def test_cube():
     new_hellopythonworld = HelloPythonWorld()
     assert new_hellopythonworld.cube(3) == 27
 
+def test_quad():
+    new_hellopythonworld = HelloPythonWorld()
+    assert new_hellopythonworld.quad(3) == 81
+
